90733627e274bccd422bf8440ec2d47b79900bf2
harleenKaur4309 <52798282+harleenKaur4309@users.noreply.github.com>
2019-07-25 20:49:42 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,28 @@
+import { BookDetails } from '../../app/bookDetails';
+
+export const MYBOOKS : BookDetails[] = [
+
+    {bname : 'Blink',
+    bauthor : 'Malcolm Gladwell',
+    bgenre : 'Non-fiction',
+    byear : 2005,
+    bpicture : 'Blink.jpg'},
+
+    {bname : 'Becoming',
+    bauthor : 'Michelle Obama',
+    bgenre : 'Memoir',
+    byear : 2018,
+    bpicture : 'Becoming.jpg'},
+
+    {bname : 'Educated',
+    bauthor : 'Tara Westover',
+    bgenre : 'Non-fiction',
+    byear : 2015,
+    bpicture : 'educated.jpeg'},
+
+    {bname : '1st to Die',
+    bauthor : 'James Patterson',
+    bgenre : 'Crime',
+    byear : 2001,
+    bpicture : '1st_to_Die.jpg'}
+]
